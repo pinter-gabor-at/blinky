fd448bcfe28d4cd3da5779adb71d14b41cb045a1
Pintér Gábor <pinter.gabor@gmx.at>
2019-04-04 12:42:15 +0200

    Better tests.

--- src/ledpattern_test.lua
+++ src/ledpattern_test.lua
@@ -1,19 +1,18 @@
 -- Test
+return function(callback)
+	local T = require("testutil")
+	T.callback = callback
 
--- If it was called with assert(loadfile("xxx_test.lua"))(callback)
--- then take the callback parameter and use it at the end of the test.
-local T = require("testutil")
-T.callback = ...
-
-
-local M = dofile("ledpattern.lua")
-node.task.post(function()
-	print("\nBlink blue LED for 5 seconds")
-	M.pattern = "010110111"
-	M.start()
-end)
-tmr.create():alarm(5000, tmr.ALARM_SINGLE, function()
-	print("Done")
-	M.stop()
-	return T.endtest(true)
-end)
+	-- Turn blue LED on and off
+	local M = dofile("ledpattern.lua")
+	node.task.post(function()
+		print("\nBlink blue LED for 5 seconds")
+		M.pattern = "010110111"
+		M.start()
+	end)
+	tmr.create():alarm(5000, tmr.ALARM_SINGLE, function()
+		print("Done")
+		M.stop()
+		return T.endtest(true)
+	end)
+end
